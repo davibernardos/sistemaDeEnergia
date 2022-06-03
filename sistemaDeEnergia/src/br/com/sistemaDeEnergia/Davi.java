f386df5bd2b233ba134f68dae1edbbc158e305af
davi.bernardo <davi.bernardo@gw-n112.cefetsc.edu.br>
2022-06-02 23:54:17 -0300

    kkkk

--- sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
+++ sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
@@ -4,6 +4,5 @@ public class Davi {
 	public void imprimeNome() {
 		System.out.println("Eu sou o Davijjj!");
 		System.out.println("Eu sou o Davijjj!");
-		System.out.println("Eu sou o Davijjjiiiiddd!");
 	}
 }
