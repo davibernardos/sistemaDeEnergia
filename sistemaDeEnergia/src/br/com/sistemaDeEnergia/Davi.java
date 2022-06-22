e799943ea74221646323a9af10ce92317c7cff10
davi.bernardo <davi.bernardo@gw-n112.cefetsc.edu.br>
2022-06-21 23:50:45 -0300

    teste commit

--- sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
+++ sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
@@ -4,5 +4,6 @@ public class Davi {
 	public void imprimeNome() {
 		System.out.println("Eu sou o Davijjj!");
 		System.out.println("Eu sou o Davijjkkkkj!");
+		System.out.println("Eu sou o Davijjkkkkj!");
 	}
 }
