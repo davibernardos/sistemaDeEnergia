558652b7800e7b778ae51f002ea81aa4605c3ddb
aluno <aluno@172.16.3.46>
2022-06-22 16:01:15 -0300

    exemplo de apagar e criar linha

--- sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
+++ sistemaDeEnergia/src/br/com/sistemaDeEnergia/Davi.java
@@ -4,10 +4,10 @@ public class Davi {
 	public void imprimeNome() {
 		System.out.println("Eu sou o Davijjj!");
 		System.out.println("Eu sou o Davijjkkkkj!");
-		System.out.println("Eu sou o Davijjkkkkj!");
 		System.out.println("Eu sou o Davijjkdddddkkkj!");
 		System.out.println("Eu sou o Davijjkdddddkkkj!");
 		System.out.println("Eu sou o Davijjkdddddkkkj!");
+		System.out.println("nova linha de teste!");
 		
 	}
 }
